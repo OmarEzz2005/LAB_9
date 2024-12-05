b4765715ce4e6b9d95a99657850e3e65f170a964
Yasein <yasein.eldeeb3@gmail.com>
2024-12-05 15:06:15 +0200

    update

--- src/FrontEnd/CreatePost.java
+++ src/FrontEnd/CreatePost.java
@@ -35,7 +35,7 @@ public class CreatePost extends javax.swing.JPanel {
         initComponents();
         jTextField2.setText("What's on Your mind ?");
         jTextField2.setForeground(Color.GRAY);
-        contentdatabase = new ContentDatabase("");
+        contentdatabase = new ContentDatabase("Content.json");
     }
 
     /**
