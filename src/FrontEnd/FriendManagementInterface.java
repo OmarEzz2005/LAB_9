44fca912cc6c15d7a8f618abe85b5c2a6da9b150
Omar Tamer Ezz <omartamer0202@gmail.com>
2024-12-06 21:18:31 +0200

    update

--- src/FrontEnd/FriendManagementInterface.java
+++ src/FrontEnd/FriendManagementInterface.java
@@ -5,6 +5,7 @@
 package frontend;
 
 
+
 import BackEnd.UserAccount;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -35,6 +36,8 @@ public class FriendManagementInterface extends javax.swing.JFrame implements Nod
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -42,6 +45,25 @@ public class FriendManagementInterface extends javax.swing.JFrame implements Nod
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
+
+        jTextField1.setText("jTextField1");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(82, 82, 82)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(107, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(66, Short.MAX_VALUE)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(65, 65, 65))
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Friend Management System");
@@ -190,7 +212,7 @@ public class FriendManagementInterface extends javax.swing.JFrame implements Nod
         return Data;
     }
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        JTextField name = new JTextField();
+      /*  JTextField name = new JTextField();
         Object[] message = {
             "Enter Username:", name
         };
@@ -208,7 +230,7 @@ public class FriendManagementInterface extends javax.swing.JFrame implements Nod
                 }
             }
 
-        }
+        }*/
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
@@ -337,6 +359,8 @@ public class FriendManagementInterface extends javax.swing.JFrame implements Nod
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 
     @Override
